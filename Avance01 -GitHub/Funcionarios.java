9c54ca705775cd33696dd4bb473b4d77be286fe1
Gustavo131997 <gustavo_mybrother@hotmail.com>
2016-09-21 19:00:12 -0300

    Update Funcionarios.java

--- Avance01 -GitHub/Funcionarios.java
+++ Avance01 -GitHub/Funcionarios.java
@@ -1,45 +1,49 @@
 package checksbus;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Funcionarios {
-    private String nombre_funcionario;
-    private String empresa_que_pertenece;
-    private String nombre_usuario;
+    private String nombrefuncionario;
+    private String empresaquepertenece;
+    private String nombreusuario;
     private String contraseña;
     
 
     public Funcionarios(String nombre_funcionario, String empresa_que_pertenece, String nombre_usuario, String contraseña) {
-        this.nombre_funcionario = nombre_funcionario;
-        this.empresa_que_pertenece = empresa_que_pertenece;
-        this.nombre_usuario = nombre_usuario;
+        this.nombrefuncionario = nombre_funcionario;
+        this.empresaquepertenece = empresa_que_pertenece;
+        this.nombreusuario = nombre_usuario;
         this.contraseña = contraseña;
     }
-    
+
     public String getNombre_funcionario() {
-        return nombre_funcionario;
+        return nombrefuncionario;
     }
 
     public void setNombre_funcionario(String nombre_funcionario) {
-        this.nombre_funcionario = nombre_funcionario;
+        this.nombrefuncionario = nombre_funcionario;
     }
     public boolean ValidarNombres(String nombre){
         Pattern pat = Pattern.compile("[A-Za-z]+");
-        Matcher mac = pat.macher(nombre);
+        Matcher mac = pat.matcher(nombre);
         return (mac.matches());
     }
+
     public String getEmpresaQuePertenece() {
-        return empresa_que_pertenece;
+        return empresaquepertenece;
     }
 
     public void setEmpresaQuePertenece(String empresa_que_pertenece) {
-        this.empresa_que_pertenece = empresa_que_pertenece;
+        this.empresaquepertenece = empresa_que_pertenece;
     }
 
     public String getNombreUsuario() {
-        return nombre_usuario;
+        return nombreusuario;
     }
 
     public void setNombreUsuario(String nombre_usuario) {
-        this.nombre_usuario = nombre_usuario;
+        this.nombreusuario = nombre_usuario;
     }
 
     public String getContraseña() {
@@ -52,6 +56,6 @@ public class Funcionarios {
 
     @Override
     public String toString() {
-        return "Funcionario \n" + "nombrefuncionario=" + nombre_funcionario + ", empresa que pertenece=" + empresa_que_pertenece + ", nombre_usuario=" + nombre_usuario + ", contrase\u00f1a=" + contraseña + '}';
+        return "Funcionario \n" + "nombrefuncionario:" + nombrefuncionario + "\t empresa que pertenece:" + empresaquepertenece + ", nombre_usuario=" + nombreusuario + ", contraseña=" + contraseña ;
     }
 }
