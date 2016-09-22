986eaf3460e42a9f34c2291cfd66f5e35cd389bd
Gustavo131997 <gustavo_mybrother@hotmail.com>
2016-09-22 17:19:00 -0300

    Update TerminalesChile.java

--- Avance01 -GitHub/TerminalesChile.java
+++ Avance01 -GitHub/TerminalesChile.java
@@ -12,22 +12,12 @@ import java.util.Scanner;
  * @author Fabian
  */
 public class TerminalesChile {
-    private static ArrayList <TerminalBuses> terminales = new ArrayList <TerminalBuses>() ;
+    private final ArrayList <TerminalBuses> terminales = new ArrayList <TerminalBuses>() ;
     static Scanner leer = new Scanner(System.in);
     
     
     public static void añadirTerminal(){
 	TerminalBuses terminal = new TerminalBuses();
-	System.out.println("Nombre terminal");
-	terminal.setNombre_terminal(leer.nextLine());			
-        System.out.println("Region");
-        terminal.setRegiondelterminal(leer.nextLine());
-        System.out.println("Direccion");
-        terminal.setDireccion(leer.nextLine());
-        terminales.add(terminal);
     }
-    
-    public static void eliminarTerminal(){
-        
-    }
+
 }
